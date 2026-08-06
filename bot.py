33e9d97158a0562144f025d73b822a8f63f767ab
Elesararagorn <elesar@tutamail.com>
2026-08-06 19:17:27 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,28 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+
+
+TOKEN = os.getenv("BOT_TOKEN")
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
         "ربات افق رویداد فعال است."
     )
 
+
 def main():
+    app = Application.builder().token(TOKEN).build()
+
+    app.add_handler(
+        CommandHandler("start", start)
+    )
+
     print("Ofogh Roydad Bot Started")
+
+    app.run_polling()
+
 
 if __name__ == "__main__":
     main()
